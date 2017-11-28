ab0c6e1bc7ecea6f4c5abfda103374194b74fa8f
Jan-Erik Rediger <janerik@fnordig.de>
2017-11-28 11:57:28 +0100

    Calculate factorial with 64-bit integers and format output

--- demos/factorial/factorial.rs
+++ demos/factorial/factorial.rs
@@ -1,11 +1,37 @@
-fn main() {}
+use std::mem;
+use std::ffi::CString;
+use std::os::raw::{c_char, c_void};
 
 #[no_mangle]
-pub fn fact(mut n: i32) -> i32 {
+pub extern "C" fn alloc(size: usize) -> *mut c_void {
+    let mut buf = Vec::with_capacity(size);
+    let ptr = buf.as_mut_ptr();
+    mem::forget(buf);
+    return ptr as *mut c_void;
+}
+
+#[no_mangle]
+pub extern "C" fn dealloc_str(ptr: *mut c_char) {
+    unsafe {
+        let _ = CString::from_raw(ptr);
+    }
+}
+
+#[no_mangle]
+pub fn fact(n: u32) -> u64 {
+    let mut n = n as u64;
     let mut result = 1;
     while n > 0 {
         result = result * n;
         n = n - 1;
     }
     result
+}
+
+#[no_mangle]
+pub fn fact_str(n: u32) -> *mut c_char {
+    let res = fact(n);
+    let s = format!("{}", res);
+    let s = CString::new(s).unwrap();
+    s.into_raw()
 }
